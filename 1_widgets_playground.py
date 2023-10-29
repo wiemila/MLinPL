3cefb3e4562c9633b39bd88e48e2d2ce1b544ff1
wiemila <134694937+wiemila@users.noreply.github.com>
2023-10-29 10:47:12 +0100

    Update 1_widgets_playground.py

--- 1_widgets_playground.py
+++ 1_widgets_playground.py
@@ -79,7 +79,7 @@ slider_value = st.slider(
     f"As a %, How much do you love {season_selected}",
     min_value=0,
     max_value=100,
-    value=100,
+    value=(20,30),
     step=5,
     format="%d%%",
     help="Pick something between 0 and 100",
