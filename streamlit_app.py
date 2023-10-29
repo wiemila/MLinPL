cf6f6571b97022b033f5eb0f28177d399a1f7bd4
wiemila <134694937+wiemila@users.noreply.github.com>
2023-10-29 09:56:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
 import streamlit as st
 st.title("welcome ML in PL 23")
+st.header("Good luck in the tutorial")
+st.subheader("at Politechnika Maths")
+
+#Play with markdown
+st.markdown("""
+Lorem**ipsum**
+-:balloon: Chemistry
+-:car: Data Science
+
+$a+a^2$
+Have a good coffee
+I am a deep learning expert 
+""", unsafe_allow_html+True)
