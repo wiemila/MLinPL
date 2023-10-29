4534d8a282120e6b3464dffcc10f4324f6263042
wiemila <134694937+wiemila@users.noreply.github.com>
2023-10-29 09:57:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,12 @@ st.subheader("at Politechnika Maths")
 #Play with markdown
 st.markdown("""
 Lorem**ipsum**
+
 -:balloon: Chemistry
 -:car: Data Science
 
 $a+a^2$
+
 Have a good coffee
 I am a deep learning expert 
 """, unsafe_allow_html=True)
